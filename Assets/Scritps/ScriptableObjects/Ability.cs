73a11838500e4dc3e44cec8dd41b609d2251fa2f
P.W <pawel.wolinski@op.pl>
2017-07-16 21:36:08 +0200

    Initial commit

--- Assets/Scritps/ScriptableObjects/Ability.cs
+++ Assets/Scritps/ScriptableObjects/Ability.cs
@@ -7,6 +7,9 @@ public abstract class Ability : ScriptableObject
     public string Name { get; private set; }
 
     public float CoolDown { get; set; }
+
+    public float CurrentHitPoints { get; set; }
+
     public abstract void UseAbility();
 
 }
